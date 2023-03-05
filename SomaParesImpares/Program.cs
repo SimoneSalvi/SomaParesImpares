5953d8363ea15d1bc2b4dfd2e80717555b969fed
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-03-05 17:52:50 -0300

    Add project files.

---
+++ SomaParesImpares/Program.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int numero = 0;
+        int somaPares = 0;
+        int somaImpares = 0;
+
+        Console.WriteLine("Informe o numero");
+        numero = int.Parse(Console.ReadLine());
+
+        while (numero < 1000)
+        {
+            if (numero % 2 == 0)
+            {
+
+                somaPares += numero;
+            }
+            else
+            {
+
+                somaImpares += numero;
+            }
+
+            Console.WriteLine("Informe o numero");
+            numero = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine("Soma pares = " + somaPares + ", e soma impares = " + somaImpares);
+
+
+
+
+
+    }
+}
